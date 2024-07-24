71615364dd1a100d69014bd143fb206db7d42184
oluwajomiloju <jomiloju45@gmail.com>
2024-07-24 15:21:18 +0100

    calculator

--- index.js
+++ index.js
@@ -19,3 +19,21 @@ function calculate(){
         display.value = "Error"
     }
 }
+
+function calculateSquareRoot() {
+    try {
+      let result = Math.sqrt(eval(display.value));
+      display.value = result.toFixed(2); // Limiting to 2 decimal places for readability
+    } catch (error) {
+      display.value = 'Error';
+    }
+  }
+  
+  function calculateSquare() {
+    try {
+      let result = Math.pow(eval(display.value), 2);
+      display.value = result.toFixed(2); // Limiting to 2 decimal places for readability
+    } catch (error) {
+      display.value = 'Error';
+    }
+  }
